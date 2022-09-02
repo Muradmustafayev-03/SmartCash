1d0308b26d1fd12becf2afd2cdb1b530a2a46233
Murad <muradmustafayev03@gmail.com>
2022-09-02 20:47:29 +0400

    ...

--- SmartCash/views.py
+++ SmartCash/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-from ML.category_classifier import SimpleClassifier
-
-c = SimpleClassifier()
 
 
 def home(request):
-    print(c.avg_precision())
     return render(request, r"purchase.html")
