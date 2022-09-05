eca3140e8065092c4da6afcdb2c0dd955c6c5361
Murad <muradmustafayev03@gmail.com>
2022-09-06 03:30:08 +0400

    dd

--- dd.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup as bs
-    
-    
-soup = bs(requests.get('https://bazarstore.az/alkoqollu-pivlr/7837-pv-kronenburg-1664-blank-460ml-').text, 'html.parser')
-
-
-print(str(soup.find("div",{"class":"product-description"}).text).strip())
