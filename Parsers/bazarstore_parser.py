61e766582667494c44dfee1a79fca0ca4234fe0d
togrul <totush04@mail.ru>
2022-09-05 16:49:56 +0400

    store parser update

--- Parsers/bazarstore_parser.py
+++ Parsers/bazarstore_parser.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import os
-from PurchasesData.models import Category, Product, Store
-from .extract_title import get_title_and_quantity
-from ML.classifiers import get_product_title, get_manufacturer
-
+from  bazarstoreDoc import ProductUnit
+# from PurchasesData.models import Category, Product, Store
+# from .extract_title import get_title_and_quantity
+# from ML.classifiers import get_product_title, get_manufacturer
 
 class BazarstoreParser:
     soup = bs(requests.get('https://bazarstore.az/').text, 'html.parser')
@@ -12,6 +12,11 @@ class BazarstoreParser:
     def get_class_head_name_as_list(self, num):
         return self.soup.find("li", {"class": f"menu-item{num}"}).find("div", {"class": "col"}).find("span").text
 
+    @staticmethod
+    def get_description_by_link(link):
+        soup = bs(requests.get(link).text, 'html.parser')
+        return (str(soup.find("div",{"class":"product-description"}).text).strip())
+        
     def get_class_child_name_as_list(self):
         classes = []
         for num in range(20):
@@ -39,12 +44,15 @@ class BazarstoreParser:
                 print(e)
         return list_of_links
 
-    @staticmethod
-    def get_every_product_from_page(page_soup):
+    def get_every_product_from_page(self, page_soup):
         dirty_titles = page_soup.find_all("h5", {"class": "product-title"})
+        
         clear_titles = []
         for i in dirty_titles:
-            clear_titles.append(i.text)
+            name = i.text
+            link = i.find("a").get("href")
+            description = self.get_description_by_link(link)
+            clear_titles.append(ProductUnit(name,link,description))
 
         return clear_titles
 
@@ -95,7 +103,7 @@ class BazarstoreParser:
         i = 0  #
         for cls, link in link_class_dict.items():
             i += 1  #
-            os.system('cls')
+            # os.system('cls')
             print(f"{i}/492")  #
             if link:
                 link_product_dict[cls] = self.parse_products_from_link(link)
@@ -127,3 +135,7 @@ def write_to_db():
                                                     quantity=1, quantity_marker='', description='', price=0)[0]
             product.save()
             product.categories.add(category)
+
+if __name__ == "__main__":
+    BazarstoreParser().get_product_and_its_class()
+    
