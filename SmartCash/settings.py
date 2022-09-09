f269bd66816b93af9cba7a329b718f426b282560
Murad <muradmustafayev03@gmail.com>
2022-09-10 01:01:49 +0400

    .

--- SmartCash/settings.py
+++ SmartCash/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5z)_8yiwha)2r5taq+ypguq2=rcus--s9zsshy=w*+on6)ce49'
+SECRET_KEY = 'django-insecure-d%z%g-o5)kd8rlgf@hxno%ma$5xf-&1r$k3e-ia-@#o)q1$)k3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
